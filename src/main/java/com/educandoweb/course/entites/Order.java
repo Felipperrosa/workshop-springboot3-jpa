29915a5413b7d4141b80baa145ef689ec58b44f1
Felipperrosa <felipperibeiro342@gmail.com>
2024-08-06 23:35:16 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/educandoweb/course/entites/Order.java
+++ src/main/java/com/educandoweb/course/entites/Order.java
@@ -6,7 +6,9 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -28,6 +30,10 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
+
 
     public Order() {
     }
@@ -70,6 +76,10 @@ public class Order implements Serializable {
         if (orderStatus != null) {
         }
         this.orderStatus = orderStatus.getCode();
+    }
+
+    public Set<OrderItem> getItems(){
+        return items;
     }
 
     @Override
--- src/main/java/com/educandoweb/course/entites/OrderItem.java
+++ src/main/java/com/educandoweb/course/entites/OrderItem.java
@@ -1,6 +1,7 @@
 package com.educandoweb.course.entites;
 
 import com.educandoweb.course.entites.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -15,7 +16,7 @@ public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
 
     private Integer quantity;
     private Double price;
@@ -30,12 +31,14 @@ public class OrderItem implements Serializable {
         this.quantity = quantity;
         this.price = price;
     }
+    @JsonIgnore
     public Order getOrder(){
         return id.getOrder();
     }
     public void setOrder(Order order){
         id.setOrder(order);
     }
+
     public Product getProduct(){
         return id.getProduct();
     }
